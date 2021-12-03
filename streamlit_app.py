65d56945f75ffda3c4912f90282c0654dcd168c7
kairntech <95371392+kairntech@users.noreply.github.com>
2021-12-03 12:41:04 +0100

    Update streamlit_app.py
    Settings for test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,5 @@ default_text = """This is a sample text.
 With two lines.
 """
 visualize(default_text,
-          # projects=["attacks_gp", "consolide"],
-          annotator_types=["plan"],
-          favorite_only=False,
-          show_connection=False,
-          project_selector_title="Select demo",
-          annotator_selector_title="Select variant",
           sidebar_title="KAIRNTECH Sherpa",
           sidebar_description="Demonstation page")
