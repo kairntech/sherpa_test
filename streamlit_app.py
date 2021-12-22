e69e591e1894b68b57c4b1dac615fa5b67b0f8a8
kairntech <95371392+kairntech@users.noreply.github.com>
2021-12-22 10:58:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ default_text = """This is a sample text.
 With two lines.
 """
 visualize(default_text,
+          show_json=True,
           sidebar_title="KAIRNTECH Sherpa",
           sidebar_description="Demonstation page")
